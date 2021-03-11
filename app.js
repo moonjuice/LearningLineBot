ad252172ad5ae23cf0d8b1dd3971b85d0976b275
moonjuice <moon80362@gmail.com>
2021-03-11 23:45:44 +0800

    using sqlite3 to record user's information

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const result = require('dotenv').config();
 if (result.error) throw result.error
 const express = require('express');
 const line = require('@line/bot-sdk');
-
 const config = {
     channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
     channelSecret: process.env.LIEN_CHANNEL_SECRET
 };
-
 const app = express();
+const file = './sqlite3.db';
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(file);
+db.run("CREATE TABLE IF NOT EXISTS  USERS (user_id TEXT, user_name TEXT)");
+var sqlStr = "SELECT * FROM USERS";
+db.each(sqlStr, function (err, row) {
+    console.log(row.user_id + ": " + row.user_name);
+});
+
+
 app.post('/webhook', line.middleware(config), (req, res) => {
     Promise
         .all(req.body.events.map(handleEvent))
@@ -20,10 +28,18 @@ const client = new line.Client(config);
 function handleEvent(event) {
     //add as friend
     if (event.type == 'follow') {
-        return client.replyMessage(event.replyToken, {
-            type: 'text',
-            text: 'your user id is: ' + event.source.userId
-        });
+        const userId = event.source.userId;
+        client.getProfile(userId)
+            .then((profile) => {
+                console.log(profile.displayName);
+                console.log(profile.userId);
+                var insertSQL = "INSERT INTO USERS (user_id, user_name) VALUES (?,?)";
+                db.run(insertSQL, [profile.userId, profile.displayName]);
+                return client.replyMessage(event.replyToken, {
+                    type: 'text',
+                    text: 'Hi ' + profile.displayName + ', your user id is: ' + userId
+                });
+            });
     }
     if (event.type !== 'message' || event.message.type !== 'text') {
         return Promise.resolve(null);
