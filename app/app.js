ed074d805ca8f2bb71290b8d62355c707191b719
moonjuice <moon80362@gmail.com>
2021-03-18 21:59:38 +0800

    add group table for join event

--- app/app.js
+++ app/app.js
@@ -17,30 +17,40 @@ var db = new sqlite3.Database(file);
 db.serialize(function () {
     db.run("CREATE TABLE IF NOT EXISTS USERS (user_id TEXT, user_name TEXT)");
     db.run("CREATE TABLE IF NOT EXISTS VERSION (db_version INT)");
-    var old_db_version = 0;
+    db.run("CREATE TABLE IF NOT EXISTS GROUPS (group_id TEXT, group_type TEXT)");
     var sqlStr = "SELECT db_version FROM VERSION";
     db.get(sqlStr, function (err, row) {
-        if (row) {
-            old_db_version = row.db_version;
-        } else {
-            db.run("INSERT INTO VERSION (db_version) VALUES (0)");
-        }
-    });
-    var new_db_version = old_db_version;
-    switch (old_db_version) {
-        case 0:
-            db.run("ALTER TABLE USERS ADD COLUMN group_id TEXT DEFAULT '0'");
-            new_db_version++;
-    }
-    if (old_db_version != new_db_version) {
-        db.run("UPDATE VERSION SET db_version = " + new_db_version + " WHERE db_version = " + old_db_version + " ");
-    }
-    console.log("DB Version: " + old_db_version + " -> " + new_db_version);
-    sqlStr = "SELECT * FROM USERS";
-    db.each(sqlStr, function (err, row) {
-        if (row) {
-            console.log(row.user_id + ": " + row.user_name + ", " + row.group_id);
-        }
+        db.serialize(function () {
+            var old_db_version = 0;
+            if (row) {
+                old_db_version = row.db_version;
+                console.log("old_db_version: " + old_db_version);
+            } else {
+                db.run("INSERT INTO VERSION (db_version) VALUES (0)");
+            }
+            var new_db_version = old_db_version;
+            switch (old_db_version) {
+                case 0:
+                    db.run("ALTER TABLE USERS ADD COLUMN group_id TEXT DEFAULT '0'");
+                    new_db_version++;
+            }
+            if (old_db_version != new_db_version) {
+                db.run("UPDATE VERSION SET db_version = " + new_db_version + " WHERE db_version = " + old_db_version + " ");
+            }
+            console.log("DB Version: " + old_db_version + " -> " + new_db_version);
+            sqlStr = "SELECT * FROM USERS";
+            db.each(sqlStr, function (err, row) {
+                if (row) {
+                    console.log(row.user_id + ": " + row.user_name + ", " + row.group_id);
+                }
+            });
+            sqlStr = "SELECT * FROM GROUPS";
+            db.each(sqlStr, function (err, row) {
+                if (row) {
+                    console.log(row.group_id + ": " + row.group_type);
+                }
+            });
+        });
     });
 });
 
@@ -74,6 +84,18 @@ function handleEvent(event) {
                     text: 'Hi ' + profile.displayName + ', your user id is: ' + userId
                 });
             });
+    }
+    //join a group or a room
+    if (event.type == 'join') {
+        const groupType = event.source.type;
+        var groupId = event.source.groupId;
+        if (!groupId) groupId = event.source.roomId;
+        var insertSQL = "INSERT INTO GROUPS (group_id, group_type) VALUES (?,?)";
+        db.run(insertSQL, [groupId, groupType]);
+        return client.replyMessage(event.replyToken, {
+            type: 'text',
+            text: 'Hello everyone! This group id is: ' + groupId
+        });
     }
     if (event.type !== 'message' || event.message.type !== 'text') {
         return Promise.resolve(null);
